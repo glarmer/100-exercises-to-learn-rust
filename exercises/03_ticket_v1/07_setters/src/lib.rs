36637f9edfb9bbbd283e70062d780f3cb349dd49
Lewis <hallrudfordlewis@gmail.com>
2024-10-21 15:20:10 +0100

    03 Finished

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -1,8 +1,3 @@
-// TODO: Add &mut-setters to the `Ticket` struct for each of its fields.
-//   Make sure to enforce the same validation rules you have in `Ticket::new`!
-//   Even better, extract that logic and reuse it in both places. You can use
-//   private functions or private static methods for that.
-
 pub struct Ticket {
     title: String,
     description: String,
@@ -11,27 +6,42 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        Self::is_title_valid(&title);
+        Self::is_description_valid(&description);
+        Self::is_status_valid(&status);
 
         Ticket {
             title,
             description,
             status,
         }
+    }
+
+    fn is_title_valid(title: &String) -> bool {
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+        true
+    }
+
+    fn is_description_valid(description: &String) -> bool {
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+        true
+    }
+
+    fn is_status_valid(status: &String) -> bool {
+        if status != "To-Do" && status != "In Progress" && status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+        true
     }
 
     pub fn title(&self) -> &String {
@@ -44,6 +54,21 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_title(&mut self, new_title: String) {
+        Self::is_title_valid(&new_title);
+        self.title = new_title
+    }
+
+    pub fn set_description(&mut self, new_description: String) {
+        Self::is_description_valid(&new_description);
+        self.description = new_description
+    }
+
+    pub fn set_status(&mut self, new_status: String) {
+        Self::is_status_valid(&new_status);
+        self.status = new_status
     }
 }
 
--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,91 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+
+    //Constructor
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        if !(Self::is_name_valid(&product_name)) {
+            panic!("The name is invalid!")
+        }
+        if !(Self::is_quantity_valid(&quantity)) {
+            panic!("The quantity is invalid!")
+        }
+        if !(Self::is_price_valid(&unit_price)) {
+            panic!("The unit price is invalid!")
+        }
+        Order {
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_name: String) {
+        if !(Self::is_name_valid(&new_name)) {
+            panic!("Name is invalid!")
+        }
+        self.product_name = new_name
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: u32) {
+        if !(Self::is_quantity_valid(&new_quantity)) {
+            panic!("Quantity is invalid!")
+        }
+        self.quantity = new_quantity
+    }
+
+    pub fn set_unit_price(&mut self, new_price: u32) {
+        if !(Self::is_price_valid(&new_price)) {
+            panic!("Price is invalid!")
+        }
+        self.unit_price = new_price
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    fn is_name_valid(name: &String) -> bool {
+        let mut valid: bool = false;
+        if !name.is_empty() && name.len() < 300 {
+            valid = true
+        }
+        valid
+    }
+
+    fn is_quantity_valid(quantity: &u32) -> bool {
+        let mut valid: bool = false;
+        if quantity > &0u32 {
+            valid = true
+        }
+        valid
+    }
+
+    fn is_price_valid(price: &u32) -> bool {
+        let mut valid: bool = false;
+        if price > &0u32 {
+            valid = true
+        }
+        valid
+    }
+}
